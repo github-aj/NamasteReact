f780c953f1e13308bd51393aa04554268cf20a73
Akshat Jaiswal <akshatjaiswal@Akshats-MacBook-Air.local>
2023-06-16 22:34:00 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h6", {
+    id :"heading6"
+}, "Welcome from React and study");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
